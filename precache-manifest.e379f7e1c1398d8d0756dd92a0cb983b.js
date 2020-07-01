1dcf3f1e173ce9b9958ce6dd954879ea0c9dba08
chetan goyal <goyalchetan078@gmail.com>
2020-07-01 19:42:29 +0530

    Updates

---
+++ precache-manifest.e379f7e1c1398d8d0756dd92a0cb983b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a995382ce84ff6692aaf71ee50bbd2ba",
+    "url": "/apitest/index.html"
+  },
+  {
+    "revision": "bb62b444958049280d85",
+    "url": "/apitest/static/css/main.44a8acaf.chunk.css"
+  },
+  {
+    "revision": "598a33b59469ea91be55",
+    "url": "/apitest/static/js/2.bb26b4a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apitest/static/js/2.bb26b4a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb62b444958049280d85",
+    "url": "/apitest/static/js/main.1618eb9e.chunk.js"
+  },
+  {
+    "revision": "51c6edfe03291974e34c",
+    "url": "/apitest/static/js/runtime-main.87676f36.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/apitest/static/media/SEGA.12a3db8b.woff"
+  }
+]);
